3e3abc36bdd77b0e5ab32348be0b6a84705de8d8
thuy nguyen <nguyenvuducthuy@gmail.com>
2019-06-11 18:22:51 +0700

    modify cmake to get correct cmake.exe

--- cmake_run.bat
+++ cmake_run.bat
@@ -1,6 +1,6 @@
 
 mkdir build
 cd build
-cmake ..\liquidfun\Box2D -G "Visual Studio 15 2017 Win64"
+D:\thuy_data\thuy_c++\cmake\bin\cmake ..\liquidfun\Box2D -G "Visual Studio 15 2017 Win64"
 REM cmake --build . --config Release
 pause
